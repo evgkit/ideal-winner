6323bd76e93c09ecd8d9d52914c341ef19d9a1a3
evgkit <kitaev.ea@gmail.com>
2018-03-30 22:26:56 +0300

    Intermediate commit

--- app/src/main/java/com/evgkit/interactivestory/MainActivity.java
+++ app/src/main/java/com/evgkit/interactivestory/MainActivity.java
@@ -2,12 +2,30 @@ package com.evgkit.interactivestory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameInput;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameInput = findViewById(R.id.nameInput);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = String.valueOf(nameInput.getText());
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
